91deebf2f12b2c32a9e397027530e920a6d8bbc5
par2hibATU <parthibbdcox@gmail.com>
2024-09-27 01:52:58 +0100

    Homework PUT method added(not working)

--- src/main/java/ie/atu/week2/week2_CICD2/ProductController.java
+++ src/main/java/ie/atu/week2/week2_CICD2/ProductController.java
@@ -1,6 +1,8 @@
 package ie.atu.week2.week2_CICD2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,12 @@ public class ProductController {
     @GetMapping("/product")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable int id, String name, double price){
+        Product updatedProduct = productService.updateProduct(id, name, price);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 
     @PostMapping("/product")
