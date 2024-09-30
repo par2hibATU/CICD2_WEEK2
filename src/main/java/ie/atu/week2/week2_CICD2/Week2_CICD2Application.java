17a33a659b2f4c513595c384153674dace0e15c4
par2hibATU <g00410769@atu.ie>
2024-09-30 17:28:27 +0100

    ports declared

--- src/main/java/ie/atu/week2/week2_CICD2/Week2_CICD2Application.java
+++ src/main/java/ie/atu/week2/week2_CICD2/Week2_CICD2Application.java
@@ -2,7 +2,9 @@ package ie.atu.week2.week2_CICD2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Week2_CICD2Application {
 
